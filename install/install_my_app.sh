a5a2c492807ccf098dac30186e62fe2cdcffa19c
farook <farookphuket@gmail.com>
2021-02-06 22:11:04 +0700

    update key

--- install/install_my_app.sh
+++ install/install_my_app.sh
@@ -2,12 +2,12 @@
 
  
 pacman -S tmux powerline gvim base-devel krusader composer --noconfirm
-pacman -S brave chromium rofi obs-studio xxkb morc_menu --noconfirm
+pacman -S brave chromium rofi filezilla obs-studio xxkb morc_menu --noconfirm
 pacman -S bmenu cmake feh picom conky-i3 nautilus youtube-dl go w3m --noconfirm
 pacman -S lxsession i3-gaps i3status-manjaro i3lock i3exit clipit --noconfirm
 pacman -S networkmanager network-manager-applet kdenlive --noconfirm
 pacman -S audacity audacious sqlitebrowser dbeaver --noconfirm
-pacman -S blueman pulseaudio pavucontrol  --noconfirm
+pacman -S blueman pulseaudio pavucontrol  nitrogen --noconfirm
 pacman -S pa-applet  lxappearance nodejs npm polkit-gnome  --noconfirm
 pacman -S xdotool xfce4-power-manager xfce4-terminal    --noconfirm
 pacman -S flameshot arandr apache php php-apache php-sqlite  --noconfirm
