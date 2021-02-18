01f15d381f17467b02a9f8958e1470b08496ef22
farook <farookphuket@gmail.com>
2021-02-18 20:59:54 +0700

    update install

--- install/install_my_app.sh
+++ install/install_my_app.sh
@@ -3,12 +3,12 @@
 
 
  
-pacman -S tmux powerline gvim base-devel krusader composer --noconfirm
+pacman -S tmux powerline gvim nvim base-devel krusader composer --noconfirm
 pacman -S brave chromium rofi filezilla obs-studio xxkb morc_menu --noconfirm
 pacman -S bmenu cmake feh picom conky-i3 nautilus youtube-dl go w3m --noconfirm
 pacman -S lxsession i3-gaps i3status-manjaro i3lock i3exit clipit --noconfirm
 pacman -S networkmanager network-manager-applet kdenlive --noconfirm
-pacman -S audacity audacious sqlitebrowser dbeaver screenkey --noconfirm
+pacman -S audacity audacious sqlitebrowser dbeaver screenkey xsel --noconfirm
 pacman -S blueman pulseaudio pavucontrol  nitrogen --noconfirm
 pacman -S pa-applet  lxappearance nodejs npm polkit-gnome  --noconfirm
 pacman -S xdotool xfce4-power-manager xfce4-terminal    --noconfirm
