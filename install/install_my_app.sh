be3264dfd8f78eeafd043428dbf8b4f8b80b7fb2
farook <farookphuket@gmail.com>
2021-02-19 09:57:51 +0700

    update install.sh file

--- install/install_my_app.sh
+++ install/install_my_app.sh
@@ -1,30 +1,86 @@
  #!/bin/bash
 
 
-
-#   Last add line 19-Feb-2021 
-pacman -S gvim neovim python-pynvim vim-clap --noconfirm
-
-pacman -S tmux powerline base-devel krusader composer --noconfirm
-pacman -S brave chromium rofi filezilla obs-studio xxkb morc_menu --noconfirm
-pacman -S bmenu cmake feh picom conky-i3 nautilus youtube-dl go w3m --noconfirm
-pacman -S lxsession i3-gaps i3status-manjaro i3lock i3exit clipit --noconfirm
-pacman -S networkmanager network-manager-applet kdenlive --noconfirm
-pacman -S audacity audacious sqlitebrowser dbeaver screenkey xsel --noconfirm
-pacman -S blueman pulseaudio pavucontrol  nitrogen --noconfirm
-pacman -S pa-applet  lxappearance nodejs npm polkit-gnome  --noconfirm
-pacman -S xdotool xfce4-power-manager xfce4-terminal    --noconfirm
-pacman -S flameshot arandr apache php php-apache php-sqlite  --noconfirm
+ #  please note that if any of the program you're not using just comment it
+ #  with (put the # sign before pacman)
 
 
-#   date 7 Feb 2021 
-pacman -S aegisub gthumb  --noconfirm
-    
-#   date 9 Feb 2021 
-pacman -S vscode atom --noconfirm
+#   need this to complie
+pacman -S  cmake base-devel go --noconfirm
 
-#   Date 17 Feb 2021 KTouch
+#   file browser 
+pacman -S  krusader nautilus --noconfirm
+
+#   web browser 
+pacman -S brave chromium  --noconfirm
+
+
+#   screen capture 
+pacman -S  obs-studio  flameshot --noconfirm
+
+#   background setting and image 
+pacman -S feh nitrogen gthumb --noconfirm
+
+#   download video from youtube
+pacman -S   youtube-dl   --noconfirm
+
+
+#   Date 17 Feb 2021 KTouch learn touch typing
 pacman -S ktouch --noconfirm
+
+#   video editing tool
+pacman -S aegisub kdenlive --noconfirm
+
+#   play sound | edit sound | show keypress on screen
+pacman -S audacity audacious  screenkey  --noconfirm
+
+
+
+
+
+#   if you're NOT use i3 window manager comment the below line 
+pacman -S rofi xxkb morc_menu --noconfirm
+
+
+pacman -S arandr networkmanager network-manager-applet --noconfirm
+pacman -S xdotool clipit xsel   --noconfirm
+pacman -S blueman pulseaudio pavucontrol pa-applet  --noconfirm
+pacman -S bmenu picom conky-i3 w3m --noconfirm
+pacman -S lxsession lxappearance --noconfirm
+pacman -S i3-gaps i3status-manjaro i3lock i3exit  --noconfirm
+
+
+echo "i3wm app has been installed"
+sleep 5s
+
+#   ---------   End of i3wm section ---------------
+
+
+
+#   if you're NOT editing the code comment the line below
+pacman -S vscode atom tmux powerline  --noconfirm
+pacman -S gvim neovim python-pynvim vim-clap tree --noconfirm
+
+echo "The code editor has been installed!"
+sleep 5s
+#   -------- End of code editor ---- program
+
+#   if your're NOT a web developper you can comment the below 3 lines
+pacman -S apache php php-apache php-sqlite filezilla composer --noconfirm
+pacman -S  sqlitebrowser dbeaver  --noconfirm
+pacman -S  nodejs npm --noconfirm
+
+echo "End of web dev install "
+
+sleep 5s
+
+
+#   ----------------------- End of web dev ------------#
+
+#   if you're on xfce you have to comment this below line 
+#   other wise this will be conflic to your system please beware
+pacman -S xfce4-power-manager xfce4-terminal polkit-gnome  --noconfirm
+
 
 
 echo "pacman has done it's operation now will install puddletag from snap"
@@ -44,4 +100,5 @@ snap install puddletag-snap
 
 
 echo "run bmenu again to fix if there are some conflic installer"
+
 
--- laptop-i3/app_list30-jan-2021/reinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for pkgName in $(cat app_30_jan_2021.txt)
-do
-    pacman -S --force --noconfirm $pkgName
-done
-
-echo "Re--Install-Successfully!"
