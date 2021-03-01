c8a3a95657cb34b36554c0df69fb73971f72d180
farook <farookphuket@gmail.com>
2021-03-01 18:21:48 +0700

    update easy setup

--- xfce_get_config.sh
+++ xfce_get_config.sh
@@ -9,8 +9,10 @@ cp -r zsh/powerlevel10k ~/
 cp zsh/.p10k.zsh ~/
 cp zsh/.zshrc ~/
 
-
+sudo  chsh -s /usr/bin/zsh 
 echo "--------------------Success config file -----------------"
+echo " your shell is $SHELL "
+echo "if your shell is not zsh please logout "
 echo " the config file has been copied! "
 
 sleep 5s
