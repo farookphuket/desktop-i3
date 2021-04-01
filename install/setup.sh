5293d4a47307b870339676a3b90b90fa4a09bbea
farook <farookphuket@gmail.com>
2021-04-01 18:03:37 +0700

    update 1 Apr 2021 fixed volume control not start

--- install/setup.sh
+++ install/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# created this file on 31 Jan 2021 via virtualbox using manjaro xfce 
-# to run this file without the root permission 
-# you can feel free to edit the file farook_use_app.sh just to install only 
-# the program that you need to use 
+# created this file on 31 Jan 2021 via virtualbox using manjaro xfce
+# to run this file without the root permission
+# you can feel free to edit the file farook_use_app.sh just to install only
+# the program that you need to use
 
 
 
@@ -11,8 +11,8 @@
 sudo -s ./pacman_install.sh
 
 
-# install with yay before snap because yay take 
-sudo -s ./yay_install.sh
+# install with yay before snap because yay take
+./yay_install.sh
 
 
 echo "-----------------------------------------------------------"
@@ -20,6 +20,6 @@ echo " Puddletah has been remove from auto install as it took"
 echo " too long to finish the process "
 echo " remove from auto install 1 Apr 2021"
 echo "-----------------------------------------------------------"
-# install from snap store 
+# install from snap store
 sudo -s ./snap_install.sh
 
